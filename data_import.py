d45816bb5c7df05cb817d731cdb93b8aed93f1a1
Harry Taegyun Yang <harry2416@gmail.com>
2019-06-04 18:16:49 -0700

    Basic Data Import and baseline test
    I finished the baseline test for predictions using genotype as well as the data import

--- data_import.py
+++ data_import.py
@@ -1,17 +1,22 @@
 import sys
 import os 
 import pandas as pd
+import numpy as np
 
 
-def test(): 
+"""
+IMPORT DATA FOR THE MODEL 
+RETURN: GENOTYPE_DF, PHENOTYPE_DF 
+"""
+def import_data_islets():
+
 	input_dir = "./DO_Islets/"
-
 	# import genotype
 	input_geno_file = "%s/Attie-232_Attie_DO_Islets-GigaMUGA_geno.csv" % (input_dir)
 
-	input_geno_df = pd.read_csv(input_geno_file, nrows = 1000, index_col = 0)
+	input_geno_df = pd.read_csv(input_geno_file, index_col = 0)
 
-	geno_cols = input_geno_df.columns.tolist()	
+	geno_cols = input_geno_df.columns.tolist()
 
 	# import phenotype
 	input_pheno_file = "%s/Attie-232_Attie_DO_Islets-GigaMUGA_pheno.csv" % (input_dir)
@@ -19,8 +24,71 @@ def test():
 	input_pheno_df = pd.read_csv(input_pheno_file, index_col = 0)
 
 	pheno_samples = input_pheno_df.index.tolist()
+	
+	# generate sample list
+	sample_list = [ x for x in geno_cols if x in pheno_samples ]
 
-	print [ x for x in geno_cols if x in pheno_samples]
+	print("Samples:")
+	print(sample_list)
+
+	# select the genotypes for the samples 
+	geno_df_select = input_geno_df[sample_list]
+
+	# select the phenotypes for the samples 
+	pheno_df_select = input_pheno_df.loc[sample_list]
+
+	return geno_df_select, pheno_df_select 
+
+def select_phenotype_islets(geno_df, pheno_df, phenotype = "num_islets"):
+	# select a phenotype for genotype baseline model 
+	# we are going to use num_islets as the test phenotype for baseline model
+	phenotype = pheno_df[phenotype]
+
+	# ID samples with missing phenotype
+	samples_with_missing_pheno = phenotype[phenotype < 0].index.tolist()
+
+	print("These samples are missing phenotypes:")
+	print(samples_with_missing_pheno)
+
+	# select samples with known phenotypes
+	phenotype_complete = pd.DataFrame(phenotype[~phenotype.index.isin(samples_with_missing_pheno)])
+
+	# save the complete_sample_list for identifying matching genotype
+	complete_sample_list = phenotype_complete.index.tolist()
+
+	# select genotype
+	genotype_complete = geno_df[complete_sample_list]
+
+	print(genotype_complete.shape)
+
+	# clean snps with missing geno 
+	genotype_complete_dropNA = genotype_complete.dropna()
+
+	print(genotype_complete_dropNA.shape)
+	print(phenotype_complete.shape)
+
+
+	return genotype_complete_dropNA, phenotype_complete
+
+def separate_training_test(geno_df, pheno_df, missing_rate = 0.1):
+
+	sample_list = geno_df.columns.to_list()
+
+	missing_num = int(0.1 * geno_df.shape[1])
+
+	sample_list_testing = np.random.choice(sample_list, int(missing_num))
+	sample_list_training = [ x for x in sample_list if x not in sample_list_testing ]
+	
+	# separate the training and test 
+	phenotype_training = pd.DataFrame(pheno_df[~pheno_df.index.isin(sample_list_testing)])
+	phenotype_testing = pd.DataFrame(pheno_df[pheno_df.index.isin(sample_list_testing)])
+
+	genotype_testing = geno_df[sample_list_testing]
+	genotype_training = geno_df[sample_list_training]
+
+	return genotype_training, phenotype_training, genotype_testing, phenotype_testing
+
+
 
 if __name__ == "__main__":
 	test()
