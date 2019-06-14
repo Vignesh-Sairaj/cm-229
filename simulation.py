ae87a0e44fcc138f68f10f557380c841ae4ceb99
Vignesh-Sairaj <vignesh.sairaj@gmail.com>
2019-06-14 16:27:42 -0700

    Fixed simulations

--- simulation.py
+++ simulation.py
@@ -7,7 +7,7 @@ def generate_B(P=15, rho=0.45):
 	'''
 	Generate the P x P AR(1) matrix with rho = 0.45
 	'''
-	return np.fromiter((rho**abs(i-j) for i in range(P) for j in range(P)), dtype=float, count=P*P).reshape((P, P))
+	return np.fromiter((rho**abs(i-j) for i in range(1, P+1) for j in range(1, P+1)), dtype=float, count=P*P).reshape((P, P))
 
 def generate_E(P=15):
 	return wishart.rvs(df=P, scale=(1.0/P)*np.eye(P))
@@ -20,8 +20,15 @@ def generate_pheno(kinship, hsquared, N=300, P=15, rho=0.45):
 	RETURNS ndarray of size (N x P)
 	'''
 
+
 	assert kinship.shape == (N, N)
-	U = matrix_normal.rvs( rowcov=kinship, colcov=hsquared*cov2corr(generate_B(P=15, rho=0.45)) )
-	epsilon = matrix_normal.rvs( rowcov=np.eye(N), colcov=(1-hsquared)*cov2corr(generate_E(P=15)) )
+
+	B = generate_B(P, rho)
+	E = generate_E(P)
+
+	chumma = np.linalg.cholesky(kinship)
+
+	U = matrix_normal.rvs( rowcov=kinship, colcov=hsquared*cov2corr(B) )
+	epsilon = matrix_normal.rvs( rowcov=np.eye(N), colcov=(1-hsquared)*cov2corr(E) )
 
 	return U + epsilon
